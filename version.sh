72e4e511497ce2d6151909ce8d62b21b19e8a8a6
Game Server Steam <game@p2h-1.as29550.net>
2013-10-04 23:19:51 +0100

    b0006.8610e55

---
+++ version.sh
@@ -0,0 +1,10 @@
+ #!/bin/sh
+
+DIR="$PWD"
+count=`git rev-list HEAD | wc -l | sed -e 's/ *//g' | xargs -n1 printf %04d`
+commit=`git show --abbrev-commit HEAD | grep '^commit' | sed -e 's/commit //'`
+buildno=b"$count.$commit"
+
+echo $buildno > "${DIR}/version.txt"
+git add .
+git commit -a -F "${DIR}/version.txt"
